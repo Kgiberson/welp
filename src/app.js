c04c82435a357d3436096c8d24215f4f361f7b1c
Kari Giberson <kari.giberson@gmail.com>
2016-10-27 16:42:09 -0700

    Add postcss processor and react/react-dom

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				This is my app!
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(<App />, document.querySelector('#root'));
