3b6978e915e9892ef4f32b4a1e4681259d193a69
Kari Giberson <kari.giberson@gmail.com>
2016-10-28 10:28:55 -0700

    Configure test and dev environments

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './app.css';
+import styles from './styles.module.css';
 
 const App = React.createClass({
 	render: function() {
 		return (
-			<div>
+			<div className={styles.wrapper}>
 				This is my app!
+				<h1>Environment: {__NODE_ENV__}</h1>
 			</div>
 		)
 	}
