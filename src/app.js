b08a0de050260fdadf650b2a1c51b4f1a2165feb
Kari Giberson <kari.giberson@gmail.com>
2016-11-11 10:08:38 -0800

    Setup react-router

--- src/app.js
+++ src/app.js
@@ -4,9 +4,27 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.css';
 import './app.css';
 
+import { browserHistory, Router, Route } from 'react-router';
+
 import App from 'containers/App/App';
 
 
+const Home = React.createClass({
+	render: function() {
+		return (<div>Hello world</div>)
+	}
+})
+
+const routes = (
+	<Router>
+		<Route path="/" component={Home} />
+	</Router>
+)
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const mountNode = document.querySelector('#root');
+
+ReactDOM.render(
+	<App history={browserHistory} routes={routes} />, mountNode
+	);
+
