ab1944c21112fc7125a9ce6b9ae763bfc755ee06
Kari Giberson <kari.giberson@gmail.com>
2016-11-28 18:48:23 -0800

    Add rating and test skeleton

---
+++ src/components/Rating/Rating.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import Rating from './Rating';
+import styles from './styles.module.css';
+
+describe('<Rating />', function () {
+	it('fills the percentage as style');
+	it('renders bottom and top star meters')
+});
