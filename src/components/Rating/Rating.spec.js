c703b6fae12fed14dee27aa755b2b95a1d7ca0e4
Kari Giberson <kari.giberson@gmail.com>
2016-11-29 17:27:07 -0800

    Add test for Rating

--- src/components/Rating/Rating.spec.js
+++ src/components/Rating/Rating.spec.js
@@ -6,6 +6,9 @@ import Rating from './Rating';
 import styles from './styles.module.css';
 
 describe('<Rating />', function () {
-	it('fills the percentage as style');
+	it('fills the percentage as style', () => {
+		let wrapper = shallow(<Rating percentage={0.10} />)
+		expect(wrapper.find(`.${styles.top}`))
+	});
 	it('renders bottom and top star meters')
 });
