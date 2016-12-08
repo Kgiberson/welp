45e0ab4e1ad0cf20fa73a96295145a2534765b3c
Kari Giberson <kari.giberson@gmail.com>
2016-12-08 10:32:20 -0800

    Detail Component

--- src/views/Main/Detail/Detail.js
+++ src/views/Main/Detail/Detail.js
@@ -1,2 +1,25 @@
 import React from 'react';
+import {getDetails} from 'utils/googleApiHelpers';
+import styles from './styles.module.css';
 
+export class Detail extends React.Component {
+	constructor(props, context) {
+		super(props, context)
+
+		this.state = {
+			loading: true,
+			place: {},
+			location: {}
+		}
+	}
+
+	getDetails(map) {}
+
+	render() {
+		return (
+			<div className={styles.details}></div>
+		)
+	}
+}
+
+export default Detail;
